e0ff176f7b7dec2f933d16d4b3748b5c0ba4b101
타래 <85001878+meowTarae@users.noreply.github.com>
2022-12-10 17:36:35 +0900

    DesignBase lecture 04

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import * as THREE from "three";
-import { CubeCamera } from "three";
-import { WEBGL } from "./webgl";
+import * as THREE from 'three'
+import { CubeCamera } from 'three'
+import { WEBGL } from './webgl'
 
 if (WEBGL.isWebGLAvailable()) {
   // 장면
-  const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x004fff);
+  const scene = new THREE.Scene()
+  scene.background = new THREE.Color(0x004fff)
 
   // 카메라
   const camera = new THREE.PerspectiveCamera(
@@ -13,25 +13,46 @@ if (WEBGL.isWebGLAvailable()) {
     window.innerWidth / window.innerHeight,
     0.1,
     1000
-  );
- 
-  // 렌더러
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  )
+  camera.position.z = 2
 
-  document.body.appendChild(renderer.domElement);
+  // 렌더러
+  const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+  })
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  document.body.appendChild(renderer.domElement)
+
+  // Mesh
+  const geometry01 = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+  const material01 = new THREE.MeshStandardMaterial({
+    color: 0x999999,
+  })
+  const obj01 = new THREE.Mesh(geometry01, material01)
+  obj01.position.x = 1
+  scene.add(obj01)
 
   function render(time) {
-    time *= 0.001;
+    time *= 0.0005
 
-    // cube.rotation.x = time;
-    // cube.rotation.y = time;
+    obj01.rotation.x = time
+    obj01.rotation.y = time
 
-    renderer.render(scene, camera);
-    requestAnimationFrame(render);
+    renderer.render(scene, camera)
+    requestAnimationFrame(render)
   }
-  requestAnimationFrame(render);
+  requestAnimationFrame(render)
+
+  // 반응형 처리
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+  }
+  window.addEventListener('resize', onWindowResize)
 } else {
-  var warning = WEBGL.getWebGLErrorMessage();
-  document.body.appendChild(warning);
+  var warning = WEBGL.getWebGLErrorMessage()
+  document.body.appendChild(warning)
 }
