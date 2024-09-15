0c4863462ece912722b768886235689ad9f7276f
타래 <85001878+meowTarae@users.noreply.github.com>
2024-09-15 14:25:16 +0900

    [🖋️study]: leva GUI

--- r3f-basic/src/App.tsx
+++ r3f-basic/src/App.tsx
@@ -1,11 +1,26 @@
 import { Canvas } from "@react-three/fiber";
 import ThreeElement from "./components/ThreeElement";
 import "./App.css";
+import { OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
 
 function App() {
+  const color = useControls({
+    value: "green",
+  });
+
+  const grid = useControls({
+    segments: { value: 10, min: 2, max: 100, step: 1 },
+  });
+
   return (
     <>
       <Canvas>
+        <color attach={"background"} args={[color.value]} />
+        <OrbitControls />
+        <axesHelper scale={[5, 5, 5]} />
+        <gridHelper args={[10, grid.segments]} />
+
         <ThreeElement />
       </Canvas>
     </>
